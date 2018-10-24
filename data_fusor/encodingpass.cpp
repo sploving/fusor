a7f693e841214a67330b57f9e8b61a0273967591
hxuhack <china.xuhui@gmail.com>
2018-10-24 14:36:32 +0000

    update

--- data_fusor/encodingpass.cpp
+++ data_fusor/encodingpass.cpp
@@ -49,6 +49,7 @@ namespace {
         }
         void EncodeGlobals(Module& M){
 	  vector<Constant*> elemptrs;
+	  vector<GlobalVariable*> gvs;
 	  Value* idx[2] = {ConstantInt::get(i32,0), ConstantInt::get(i32,0)};
 	  int size = 0;
 	  for(auto &iter : M.getGlobalList()){
@@ -57,10 +58,8 @@ namespace {
 		if(data->isString()){
 		  Constant* ptr = ConstantExpr::getInBoundsGetElementPtr(data->getType(), &iter, ArrayRef<Value *>(idx,2));
 		  elemptrs.emplace_back(ptr);
+		  gvs.emplace_back(&iter);
 	    	  errs() << data->getAsString() << "\n";
-	    	  for(auto use : iter.users()){
-	 	    errs() << *use << "\n";
-		  }
 		  size ++;
 		}
 	      }
@@ -70,6 +69,27 @@ namespace {
 	  ArrayType* atype = ArrayType::get(i8p, size);
 	  Constant* elements = ConstantArray::get(atype, ArrayRef<Constant*> (elemptrs));
 	  auto* array = new GlobalVariable(M, (Type *) atype, false, GlobalValue::ExternalLinkage, elements, "fusor_strs");
+
+	  for(auto &gv : gvs){
+	    int offset = 0;
+	    Value* idx_dyn[2] = {ConstantInt::get(i32,0), ConstantInt::get(i32,offset)};
+	    bool hasInserted = false;
+	    for(auto user : gv->users()){
+	      errs() << "User:" << *user << "\n";
+	      if(!hasInserted){
+	          Value* tmp = dyn_cast<Value>(user);
+	          Instruction* insertBefore = dyn_cast<Instruction>(tmp->user_back());
+	          LoadInst* inst; 
+		  if(insertBefore){
+	            errs() << "User:" << *insertBefore << "\n";
+	            inst = new LoadInst(ConstantExpr::getInBoundsGetElementPtr(atype, array, ArrayRef<Value*>(idx_dyn)),"fusor_load",insertBefore); 
+	            hasInserted = true;
+		  } 
+		  user = dyn_cast<User>(inst);
+	      }
+	    }
+	    offset++;
+	  }
 	}
         Type *i8 = Type::getInt8Ty(getGlobalContext());
         Type *i8p = Type::getInt8PtrTy(getGlobalContext());
